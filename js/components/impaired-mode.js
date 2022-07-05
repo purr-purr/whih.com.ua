f7992c25bd4bf9cda3b3625dea19383ae079d4fe
Shaposhnikov Anton <56505654+purr-purr@users.noreply.github.com>
2022-07-05 17:49:38 +0100

    via upload project files

---
+++ js/components/impaired-mode.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#impaired-mode-btn').on('click', function () {
+        console.log("impaired-mode");
+        $('body').addClass('impaired-mode, a2');
+    });
+});
---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Import HTML template
+$(function () {
+    // layout
+    $("#included__header").load("/modules/header/index.html");
+    $("#included__footer").load("/modules/footer/index.html");
+    // any
+    $("#included__contact-block").load("/modules/contactBlock/index.html");
+    $("#included__preloader").load("/modules/preloader/index.html");
+});
+// ---------------------------------------------------------------------------
+
+// Прелоадер
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+}
+
+$(document).ready(function () {
+    $('#home-desc__read-more-btn').on('click', function () {
+        $('.home-desc__info--hide-text').css('display', 'block');
+        $(this).hide();
+    });
+});
+
